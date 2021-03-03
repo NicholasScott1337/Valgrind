477c457e0e1e16d7ec8992f9d071a202739e1ff0
NicholasScott1337 <daymonforeman14@gmail.com>
2021-03-03 03:30:57 -0600

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valgrind
+{
+    [BepInPlugin("com.nicholascott.valheim.valgrindclient", "Valgrind", "0.0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource LOG;
+        public void Awake()
+        {
+            LOG = this.Logger;
+            var x = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly()).GetPatchedMethods().ToArray().Length;
+            base.Logger.LogInfo($"Patched {x} method{(x > 1 ? "s" : "")}");
+        }
+    }
+}
