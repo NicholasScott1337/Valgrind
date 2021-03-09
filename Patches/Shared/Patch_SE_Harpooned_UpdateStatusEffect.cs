b32e6b3a4ea36b539f52527754261fe2b94ef7c8
NicholasScott1337 <daymonforeman14@gmail.com>
2021-03-09 06:13:46 -0600

    Fixes

---
+++ Patches/Shared/Patch_SE_Harpooned_UpdateStatusEffect.cs
@@ -0,0 +1,31 @@
+﻿using SmartBepInMods.Tools.Transpiling;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using System.Reflection.Emit;
+
+namespace Valgrind.Patches.Shared
+{
+    [HarmonyPatch(typeof(SE_Harpooned), "UpdateStatusEffect", null)]
+    class Patch_SE_Harpooned_UpdateStatusEffect : SmartBepInMods.Tools.Patching.Constants.SHARED
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return instructions.SmartNopAll(new KeyValuePair<CodeInstruction, Qualifier>[]
+            {
+                new KeyValuePair<CodeInstruction, Qualifier>(new CodeInstruction(OpCodes.Ldarg_0), Qualifier.Default),
+                new KeyValuePair<CodeInstruction, Qualifier>(new CodeInstruction(OpCodes.Ldfld), Qualifier.Default),
+                new KeyValuePair<CodeInstruction, Qualifier>(new CodeInstruction(OpCodes.Callvirt), Qualifier.Default),
+                new KeyValuePair<CodeInstruction, Qualifier>(new CodeInstruction(OpCodes.Ldnull), Qualifier.Default),
+                new KeyValuePair<CodeInstruction, Qualifier>(new CodeInstruction(OpCodes.Call), Qualifier.Default),
+                new KeyValuePair<CodeInstruction, Qualifier>(new CodeInstruction(OpCodes.Brfalse_S), Qualifier.Default),
+                new KeyValuePair<CodeInstruction, Qualifier>(new CodeInstruction(OpCodes.Ldarg_0), Qualifier.Default),
+                new KeyValuePair<CodeInstruction, Qualifier>(new CodeInstruction(OpCodes.Ldfld), Qualifier.Default),
+                new KeyValuePair<CodeInstruction, Qualifier>(new CodeInstruction(OpCodes.Callvirt), Qualifier.Default),
+                new KeyValuePair<CodeInstruction, Qualifier>(new CodeInstruction(OpCodes.Brtrue_S), Qualifier.Default),
+            });
+        }
+    }
+}
